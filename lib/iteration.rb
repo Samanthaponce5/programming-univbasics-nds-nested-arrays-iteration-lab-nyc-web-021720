f6f9f5bf3be468aa35d7a148d22cdd74a9f7b19a
Samantha Ponce <samanthaponce5@gmail.com>
2020-02-06 18:43:18 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -38,8 +38,22 @@ end
 
 
 def total_even_pairs(src)
-src
+  total = 0
+  i = 0
+  while i < src.length
+   inner = 0 
+   while inner < src[i].length
+     if src[i][0] % 2 == 0 && src[i][1] % 2 == 0
+  total += src[i][inner]
+    end
+   inner += 1
+  end
+   i += 1
+  end
+  total
 end
+
+total_even_pairs(src)
     
 
 
