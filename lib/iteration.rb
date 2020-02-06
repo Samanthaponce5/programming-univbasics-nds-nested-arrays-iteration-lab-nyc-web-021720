ca69ea98ad7f6dd63f2b40501b4e5854f066b331
Samantha Ponce <samanthaponce5@gmail.com>
2020-02-06 18:43:40 +0000

    Done.

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -1,31 +1,32 @@
-def join_ingredients(src)
+
   # Given an Array of 2-element Arrays ( [ [food1, food2], [food3, # food4]....[foodN, foodM]]):
   # Build a new Array that contains strings where each pair of foods is
   # inserted into this template:
   # "I love (inner array element 0) and (inner array element 1) on my pizza""
   # As such, there should be a new String for each inner array, or pair
  
-   outer_results = []
-  row_index = 0
-  while row_index < src.count do
-    inner = src[row_index]
-    outer_results << "I love #{inner[0]} and #{inner[1]} on my pizza"
-    row_index += 1
-  end
-  outer_results
+  def join_ingredients(src)
+ i = 0 
+ new = []
+ while i < src.length
+    new << "I love #{src[i][0]} and #{src[i][1]} on my pizza"
+  i += 1
+ end
+new
 end
 
 
+
 def find_greater_pair(src)
-  result = []
-  i = 0
-  while i < src.count do
-   
-    result << src[i].max
-    i += 1
-    end
-result
+  arr = []
+ i = 0 
+ while i < src.length
+arr << src[i].max
+   i += 1
   end
+arr
+end
+  
  
         
         
@@ -37,28 +38,24 @@ result
 
 
 def total_even_pairs(src)
-  new_array = 0
-  i = 0 
-  while i < src.count do
-    if src[i][0] % 2 == 0 && src[i][1] % 2 == 0 
-    new_array += src[i][0] + src[i][1]
+  total = 0
+  i = 0
+  while i < src.length
+   inner = 0 
+   while inner < src[i].length
+     if src[i][0] % 2 == 0 && src[i][1] % 2 == 0
+  total += src[i][inner]
+    end
+   inner += 1
   end
-    i += 1 
+   i += 1
   end
-new_array
+  total
 end
+
+
     
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+
 
   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
   # if both numbers in the pair are even, then add both those numbers to the
